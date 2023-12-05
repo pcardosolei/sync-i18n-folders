984113c28a262243cc55c5293710f4f1257b2825
Paulo Gonçalves Cardoso <pcardoso.lei@gmail.com>
2023-12-05 17:22:03 +0000

    adding more tests to be added

--- __tests__/json.spec.ts
+++ __tests__/json.spec.ts
@@ -1,19 +1,33 @@
 import * as path from 'path';
 import * as fs from 'fs';
 
-// import sync from '../src/index';
+import sync from '../src/index';
 
-// const LOCALES_DIR = path.join(__dirname, 'locales');
-// const OUTPUT_DIR = path.join(__dirname, 'output');
+const LOCALES_DIR = path.join(__dirname, 'locales');
+const OUTPUT_DIR = path.join(__dirname, 'output');
 
-// afterEach(() => {
-//   cleanUpOutputFiles();
-// });
+afterEach(() => {
+  cleanUpOutputFiles();
+});
 
-// describe('checking folder and files creation', () => {
+describe('using boilerplate', () => {
+  test('singular key with string', () => {});
+  test('key with array', () => {});
+  test('key with nested object', () => {});
 
-// });
+  test('key in two different files are different types', () => {});
+  test('key does not exist in one of the files', () => {});
+});
 
-// function cleanUpOutputFiles() {
-//   fs.rm(OUTPUT_DIR, { recursive: true }, () => {});
-// }
+describe('with boilerplate turned off', () => {
+  test('singular key with string', () => {});
+  test('key with array', () => {});
+  test('key with nested object', () => {});
+
+  test('key in two different files are different types', () => {});
+  test('key does not exist in one of the files', () => {});
+});
+
+function cleanUpOutputFiles() {
+  fs.rm(OUTPUT_DIR, { recursive: true }, () => {});
+}
